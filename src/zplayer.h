19ac777d771f109ca809b90fffa6b0bc1d8367fe
zhengzhenhao <zhengzhenhao@axzt.net>
2024-06-17 11:36:25 +0800

    refactor: add libzplayer

--- src/zplayer.h
+++ src/zplayer.h
@@ -1,16 +1,17 @@
 #pragma once
 
+#define EXPORT __declspec(dllexport)
+
 enum MsgType {
     MsgType_DurationMs,
     MsgType_CurrentTimestampMs
 };
 
-void* zplayer_create(void* surface);
-void zplayer_open(void* zplayer, const char* url);
-void zplayer_close(void** zplayer);
-void zplayer_play(void* zplayer);
-void zplayer_pause(void* zplayer);
-void zplayer_seek(void* zplayer, int timestamp);
-void zplayer_render(void* zplayer, void* frame);
-
-void zplayer_query(void* zplayer, MsgType msgtype, int* ret);
+EXPORT void* zplayer_create(void* surface);
+EXPORT void zplayer_open(void* zplayer, const char* url);
+EXPORT void zplayer_close(void** zplayer);
+EXPORT void zplayer_play(void* zplayer);
+EXPORT void zplayer_pause(void* zplayer);
+EXPORT void zplayer_seek(void* zplayer, int timestamp);
+EXPORT void zplayer_render(void* zplayer, void* frame);
+EXPORT void zplayer_query(void* zplayer, MsgType msgtype, int* ret);
